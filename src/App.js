fae323b419f71e8fb784c1c0ab8bdc056e0e1e65
SUNIL NUTHALAPATI <SUNIL_C_NUTHALAPATI@homedepot.com>
2020-06-02 11:50:07 -0500

    covid 19 resume

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello world </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div className="c">
+      <div className="row">
+        <h1 className="10 col">
+          COVID 19{" "}
+          <span role="img" aria-label="covid 19 emoji">
+            😷
+          </span>
+        </h1>
+        <h6 className="2 col">facepalm@iamscrewed.com</h6>
+      </div>
+      <hr />
+      <h4>Summary</h4>
+      <ul>
+        <li>Around 6 months experience in global pandemic and delivered enormous amount of chaos</li>
+        <li>Successfully delivered 375K deaths and infected 6.26M people world wide.</li>
+        <li>Exposed weaknesses of humanity and made them fight for toilet paper</li>
+        <li>Implemented global standards for eating, greeting, talking, walking, and working</li>
+        <li>Experience in continuous infections and continuous deaths</li>
+      </ul>
+      <h4>Technical Skills</h4>
+      <ul>
+        <li>Fever or chills</li>
+        <li>Cough</li>
+        <li>Shortness of breath or difficulty breathing</li>
+        <li>Fatigue</li>
+        <li>Muscle or body aches</li>
+        <li>Headache</li>
+        <li>New loss of taste or smell</li>
+        <li>Sore throat</li>
+        <li>Congestion or runny nose</li>
+        <li>Nausea or vomiting</li>
+        <li>Diarrhea</li>
+      </ul>
+      <h4>Education</h4>
+      <p>Bachelor of Pandemic, University of Wet Markets, Wuhan, China </p>
+      <p>Master of Death, University of Subway Stations, New York, USA</p>
     </div>
   );
 }
