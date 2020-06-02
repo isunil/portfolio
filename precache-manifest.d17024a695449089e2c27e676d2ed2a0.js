ad661d538c024fcd9e0a93af141d486d0fea53c8
isunil <sunil.nuthalapati@gmail.com>
2020-06-02 16:43:28 +0000

    Updates

---
+++ precache-manifest.d17024a695449089e2c27e676d2ed2a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d879003794dca8f98bc8ebd506ed27bb",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a8449a8e80b4f345d3d7",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d3a51ed187374089e7d0",
+    "url": "/portfolio/static/js/2.9e384841.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.9e384841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8449a8e80b4f345d3d7",
+    "url": "/portfolio/static/js/main.c78d9c65.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
